458d71f78625c5e0f10f35436d3fb4c3bad2f2fc
rinklepoonia <rinklepoonia889@gmail.com>
2024-06-04 15:19:56 +0530

    still-unsure-accordian done

--- src/components/StillUnsure.jsx
+++ src/components/StillUnsure.jsx
@@ -1,51 +1,128 @@
 import React from 'react'
 
 function StillUnsure() {
-  return (
-      <div>
-          <div className="container">
-              <h2 className='ff_titling fw-bold  text48_2xl lh_lg_57 lh_39 color_blue text-center mb-5'>Still Unsure? Check Out These FAQs</h2>
-              <div class="accordion" id="accordionExample">
-                  <div class="accordion-item">
-                      <h2 class="accordion-header" id="headingOne">
-                          <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                              Accordion Item #1
-                          </button>
-                      </h2>
-                      <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                          <div class="accordion-body">
-                              <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-                          </div>
-                      </div>
-                  </div>
-                  <div class="accordion-item">
-                      <h2 class="accordion-header" id="headingTwo">
-                          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                              Accordion Item #2
-                          </button>
-                      </h2>
-                      <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-                          <div class="accordion-body">
-                              <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-                          </div>
-                      </div>
-                  </div>
-                  <div class="accordion-item">
-                      <h2 class="accordion-header" id="headingThree">
-                          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                              Accordion Item #3
-                          </button>
-                      </h2>
-                      <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-                          <div class="accordion-body">
-                              <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-    </div>
-  )
+    return (
+        <div>
+            <div className="container">
+                <h2 className='ff_titling fw-bold  text48_2xl lh_lg_57 lh_39 color_blue text-center mb-5'>Still Unsure? Check Out These FAQs</h2>
+                <div class="accordion" id="accordionExample">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingOne">
+                            <button class="accordion-button ff_acmin fw-bold lh_33" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                What types of vehicles are best suited for dry ice blasting?
+                            </button>
+                        </h2>
+                        <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                            <div class="accordion-body ff_acmin fw-normal text16_xs lh_24">
+                                Dry ice blasting is effective for cleaning a wide range of surfaces, including aircraft, automotive components, railway equipment, marine vessels, industrial machinery, food processing equipment, electrical components, and historic structures. The method is versatile, non-abrasive, and environmentally friendly, making it suitable for diverse applications across various industries.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingTwo">
+                            <button class="accordion-button collapsed ff_acmin fw-bold lh_33" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                How often should I use dry ice blasting for optimal vehicle maintenance?
+                            </button>
+                        </h2>
+                        <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                            <div class="accordion-body ff_acmin fw-normal text16_xs lh_24">
+                                Dry ice blasting is effective for cleaning a wide range of surfaces, including aircraft, automotive components, railway equipment, marine vessels, industrial machinery, food processing equipment, electrical components, and historic structures. The method is versatile, non-abrasive, and environmentally friendly, making it suitable for diverse applications across various industries.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingThree">
+                            <button class="accordion-button collapsed ff_acmin fw-bold lh_33" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                Can dry ice blasting be used on vehicle interiors?
+                            </button>
+                        </h2>
+                        <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                            <div class="accordion-body ff_acmin fw-normal text16_xs lh_24">
+                                Dry ice blasting is effective for cleaning a wide range of surfaces, including aircraft, automotive components, railway equipment, marine vessels, industrial machinery, food processing equipment, electrical components, and historic structures. The method is versatile, non-abrasive, and environmentally friendly, making it suitable for diverse applications across various industries.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingFoure">
+                            <button class="accordion-button collapsed ff_acmin fw-bold lh_33" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFoure" aria-expanded="false" aria-controls="collapseFoure">
+                                Can dry ice blasting be used on vehicle interiors?
+                            </button>
+                        </h2>
+                        <div id="collapseFoure" class="accordion-collapse collapse" aria-labelledby="headingFoure" data-bs-parent="#accordionExample">
+                            <div class="accordion-body ff_acmin fw-normal text16_xs lh_24">
+                                Dry ice blasting is effective for cleaning a wide range of surfaces, including aircraft, automotive components, railway equipment, marine vessels, industrial machinery, food processing equipment, electrical components, and historic structures. The method is versatile, non-abrasive, and environmentally friendly, making it suitable for diverse applications across various industries.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingFive">
+                            <button class="accordion-button collapsed ff_acmin fw-bold lh_33" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                                Can dry ice blasting be used on vehicle interiors?
+                            </button>
+                        </h2>
+                        <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
+                            <div class="accordion-body ff_acmin fw-normal text16_xs lh_24">
+                                Dry ice blasting is effective for cleaning a wide range of surfaces, including aircraft, automotive components, railway equipment, marine vessels, industrial machinery, food processing equipment, electrical components, and historic structures. The method is versatile, non-abrasive, and environmentally friendly, making it suitable for diverse applications across various industries.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingSix">
+                            <button class="accordion-button collapsed ff_acmin fw-bold lh_33" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+                                Can dry ice blasting be used on vehicle interiors?
+                            </button>
+                        </h2>
+                        <div id="collapseSix" class="accordion-collapse collapse" aria-labelledby="headingSix" data-bs-parent="#accordionExample">
+                            <div class="accordion-body ff_acmin fw-normal text16_xs lh_24">
+                                Dry ice blasting is effective for cleaning a wide range of surfaces, including aircraft, automotive components, railway equipment, marine vessels, industrial machinery, food processing equipment, electrical components, and historic structures. The method is versatile, non-abrasive, and environmentally friendly, making it suitable for diverse applications across various industries.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingSeven">
+                            <button class="accordion-button collapsed ff_acmin fw-bold lh_33" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+                                Can dry ice blasting be used on vehicle interiors?
+                            </button>
+                        </h2>
+                        <div id="collapseSeven" class="accordion-collapse collapse" aria-labelledby="headingSeven" data-bs-parent="#accordionExample">
+                            <div class="accordion-body ff_acmin fw-normal text16_xs lh_24">
+                                Dry ice blasting is effective for cleaning a wide range of surfaces, including aircraft, automotive components, railway equipment, marine vessels, industrial machinery, food processing equipment, electrical components, and historic structures. The method is versatile, non-abrasive, and environmentally friendly, making it suitable for diverse applications across various industries.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingEight">
+                            <button class="accordion-button collapsed ff_acmin fw-bold lh_33" type="button" data-bs-toggle="collapse" data-bs-target="#collapseEight" aria-expanded="false" aria-controls="collapseEight">
+                                Can dry ice blasting be used on vehicle interiors?
+                            </button>
+                        </h2>
+                        <div id="collapseEight" class="accordion-collapse collapse" aria-labelledby="headingEight" data-bs-parent="#accordionExample">
+                            <div class="accordion-body ff_acmin fw-normal text16_xs lh_24">
+                                Dry ice blasting is effective for cleaning a wide range of surfaces, including aircraft, automotive components, railway equipment, marine vessels, industrial machinery, food processing equipment, electrical components, and historic structures. The method is versatile, non-abrasive, and environmentally friendly, making it suitable for diverse applications across various industries.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingNine">
+                            <button class="accordion-button collapsed ff_acmin fw-bold lh_33" type="button" data-bs-toggle="collapse" data-bs-target="#collapseNine" aria-expanded="false" aria-controls="collapseNine">
+                                Can dry ice blasting be used on vehicle interiors?
+                            </button>
+                        </h2>
+                        <div id="collapseNine" class="accordion-collapse collapse" aria-labelledby="headingNine" data-bs-parent="#accordionExample">
+                            <div class="accordion-body ff_acmin fw-normal text16_xs lh_24">
+                                Dry ice blasting is effective for cleaning a wide range of surfaces, including aircraft, automotive components, railway equipment, marine vessels, industrial machinery, food processing equipment, electrical components, and historic structures. The method is versatile, non-abrasive, and environmentally friendly, making it suitable for diverse applications across various industries.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='diffQues_box'>
+                    <p className='ff_acmin fw-bold text24_2md lh_33 color_blue'>Have a Different Question? Contact Us Today</p>
+                    <textarea name="" id="" className='ff_acmin fw-normal text16_xs lh_24 color_greyBorder outline_none w-100 white_input_box' placeholder='Type something...'></textarea>
+                    <button className='ff_acmin fw-bold text16_xs lh_19 color_white btn_property mt_md_40 mt_10'>Submit Now</button>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default StillUnsure
