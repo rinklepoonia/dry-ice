43c2ac7a7d2679eb38fd507ce023a91a47392c9c
rinklepoonia <rinklepoonia889@gmail.com>
2024-06-26 16:39:03 +0530

    apply-video-link

--- src/components/BehindSection.jsx
+++ src/components/BehindSection.jsx
@@ -1,27 +1,57 @@
-import React from 'react'
-import behindCarImg from '../assets/images/webp/behind-car-img.webp'
-import layer3 from '../assets/images/webp/layer3.webp'
-import layer4 from '../assets/images/webp/layer4.webp'
-import playBtn from '../assets/images/webp/playbutton.webp'
+import React from "react";
+import behindCarImg from "../assets/images/webp/behind-car-img.webp";
+import layer3 from "../assets/images/webp/layer3.webp";
+import layer4 from "../assets/images/webp/layer4.webp";
+import playBtn from "../assets/images/webp/playbutton.webp";
 
 function BehindSection() {
-    return (
-        <div className='behind_bg_img'>
-            <img className='w-100 mt_2' src={layer3} alt="layer3" />
-            <div className="container">
-                <h2 className='ff_titling fw-bold text48_2xl lh_lg_57 lh_39 color_white text-center mx-auto mb-2 pb-1 pt-lg-5 pt-2 mt-lg-4 mt-0'>Behind the Scenes
-                    <span className='d-lg-block'>Dry Ice Blasting a Car</span></h2>
-                <p className='ff_acmin fw-normal text16_xs lh_24 color_white text-center mb-lg-5 mb-3'>From the initial setup to the final sweep, discover how we elevate car cleaning to an <span className='d-lg-block'>art form, delivering immaculate results every time.</span></p>
-                <div data-aos="zoom-in" className='position-relative d-flex justify-content-xl-center'>
-                    <img className='behind_img_width' src={behindCarImg} alt="behindCarImg" />
-                    <div className='position-absolute pos_playbtn'><img className='max_w_md_60 max_w_25 cursor_pointer' src={playBtn} alt="playBtn" /></div>
-                </div>
-                <p className='ff_acmin fw-bold text16_xs lh_24 color_white mt-md-4 mt-3 pt-lg-3 pt-0 text-center mb-md-4 mb-2'>Discover a cleaner, fresher, and more beautiful car, inside and out, with our advanced dry ice blasting services.</p>
-                <div className='text-center mb-lg-3 mb-0'><button className='ff_acmin fw-bold text16_xs lh_19 color_white btn_property btn_hover transition'>Get Instant Quote</button></div>
+  return (
+    <div className="behind_bg_img">
+      <img className="w-100 mt_2" src={layer3} alt="layer3" />
+      <div className="container">
+        <h2 className="ff_titling fw-bold text48_2xl lh_lg_57 lh_39 color_white text-center mx-auto mb-2 pb-1 pt-lg-5 pt-2 mt-lg-4 mt-0">
+          Behind the Scenes
+          <span className="d-lg-block">Dry Ice Blasting a Car</span>
+        </h2>
+        <p className="ff_acmin fw-normal text16_xs lh_24 color_white text-center mb-lg-5 mb-3">
+          From the initial setup to the final sweep, discover how we elevate car
+          cleaning to an{" "}
+          <span className="d-lg-block">
+            art form, delivering immaculate results every time.
+          </span>
+        </p>
+        <a href="https://youtu.be/EM_FKck0DTo?feature=shared" target="_blank">
+          <div
+            data-aos="zoom-in"
+            className="position-relative d-flex justify-content-xl-center"
+          >
+            <img
+              className="behind_img_width"
+              src={behindCarImg}
+              alt="behindCarImg"
+            />
+            <div className="position-absolute pos_playbtn">
+              <img
+                className="max_w_md_60 max_w_25 cursor_pointer"
+                src={playBtn}
+                alt="playBtn"
+              />
             </div>
-            <img className='w-100 mb_1' src={layer4} alt="layer4" />
+          </div>
+        </a>
+        <p className="ff_acmin fw-bold text16_xs lh_24 color_white mt-md-4 mt-3 pt-lg-3 pt-0 text-center mb-md-4 mb-2">
+          Discover a cleaner, fresher, and more beautiful car, inside and out,
+          with our advanced dry ice blasting services.
+        </p>
+        <div className="text-center mb-lg-3 mb-0">
+          <button className="ff_acmin fw-bold text16_xs lh_19 color_white btn_property btn_hover transition">
+            Get Instant Quote
+          </button>
         </div>
-    )
+      </div>
+      <img className="w-100 mb_1" src={layer4} alt="layer4" />
+    </div>
+  );
 }
 
-export default BehindSection
+export default BehindSection;
--- src/components/Future.jsx
+++ src/components/Future.jsx
@@ -5,58 +5,103 @@ import playBtn from '../assets/images/webp/playbutton.webp'
 
 function Future() {
     return (
-        <div className='pt_lg_83 pt_40 future_bg_img bg_no_repate w-100'>
-            <div className="container">
-                <div className="row flex-lg-row flex-column-reverse">
-                    <div data-aos="fade-right" className="col-lg-6 pt-lg-0 pt-4">
-                        <h2 className='ff_titling fw-bold text48_2xl lh_lg_57 lh_39 color_blue max_w_351 mb-lg-2 mb-1'>The Future of<span className='d-lg-block'> Car Cleaning</span></h2>
-                        <p className='ff_acmin fw-normal text16_xs lh_24 color_blue opacity_70 max_w_558 mb-lg-4 mb-md-3 mb-2'>No need to be overwhelmed by the thought of harsh chemicals and abrasive cleaning methods. Our dry ice blasting leaves your car spotless, reaching where no cloth can.</p>
-                        <div className='d-flex align-items-start gap-3 mb-md-3 mb-2'>
-                            <img src={tiksvg} alt="tiksvg" />
-                            <div className='blank'>
-                                <p className='ff_acmin fw-bold text16_xs lh_24 color_blue mb-md-1 mb-0'>Gentle Yet Powerful</p>
-                                <p className='ff_acmin fw-normal text16_xs lh_24 color_blue opacity_70 max_w_518 mb-0'>Our dry ice method lifts dirt effortlessly without the need for harsh chemicals or abrasive techniques.</p>
-                            </div>
-                        </div>
-                        <div className='d-flex align-items-start gap-3 mb-md-3 mb-2'>
-                            <img src={tiksvg} alt="tiksvg" />
-                            <div className='blank'>
-                                <p className='ff_acmin fw-bold text16_xs lh_24 color_blue mb-md-1 mb-0'>Precision Cleaning</p>
-                                <p className='ff_acmin fw-normal text16_xs lh_24 color_blue opacity_70 max_w_518 mb-0'>See how accurately we target the grunge zones, ensuring every inch of your car's undercarriage and engine is meticulously cleaned.</p>
-                            </div>
-                        </div>
-                        <div className='d-flex align-items-start gap-3 mb-md-3 mb-2'>
-                            <img src={tiksvg} alt="tiksvg" />
-                            <div className='blank'>
-                                <p className='ff_acmin fw-bold text16_xs lh_24 color_blue mb-md-1 mb-0'>Safe for AlI Vehicles</p>
-                                <p className='ff_acmin fw-normal text16_xs lh_24 color_blue opacity_70 max_w_518 mb-0'>Perfect for all car types, from family hatchbacks to classic cars and high-performance vehicles.</p>
-                            </div>
-                        </div>
-                        <div className='d-flex align-items-start gap-3 mb-md-3 mb-0 pb-lg-0 pb-sm-4 pb-3'>
-                            <img src={tiksvg} alt="tiksvg" />
-                            <div className='blank'>
-                                <p className='ff_acmin fw-bold text16_xs lh_24 color_blue mb-md-1 mb-0'>Eco-Friendly Approach</p>
-                                <p className='ff_acmin fw-normal text16_xs lh_24 color_blue opacity_70 max_w_518 mb-0'>Notice the absence of water or chemical runoff, showcasing our commitment to an environmentally responsible cleaning process.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div data-aos="fade-left" className="col-lg-6 d-flex justify-content-center">
-                        <div className='position-relative'>
-                            <img className='max_w_558 max_w_sm_400 max_w_280 w-100 h-100 object-fit-contain' src={futureCleaning} alt="futureCleaning" />
-                            <div className='position-absolute pos_playbtn'><img className='max_w_md_60 max_w_25 cursor_pointer' src={playBtn} alt="playBtn" /></div>
-                        </div>
-                    </div>
+      <div className="pt_lg_83 pt_40 future_bg_img bg_no_repate w-100">
+        <div className="container">
+          <div className="row flex-lg-row flex-column-reverse">
+            <div data-aos="fade-right" className="col-lg-6 pt-lg-0 pt-4">
+              <h2 className="ff_titling fw-bold text48_2xl lh_lg_57 lh_39 color_blue max_w_351 mb-lg-2 mb-1">
+                The Future of<span className="d-lg-block"> Car Cleaning</span>
+              </h2>
+              <p className="ff_acmin fw-normal text16_xs lh_24 color_blue opacity_70 max_w_558 mb-lg-4 mb-md-3 mb-2">
+                No need to be overwhelmed by the thought of harsh chemicals and
+                abrasive cleaning methods. Our dry ice blasting leaves your car
+                spotless, reaching where no cloth can.
+              </p>
+              <div className="d-flex align-items-start gap-3 mb-md-3 mb-2">
+                <img src={tiksvg} alt="tiksvg" />
+                <div className="blank">
+                  <p className="ff_acmin fw-bold text16_xs lh_24 color_blue mb-md-1 mb-0">
+                    Gentle Yet Powerful
+                  </p>
+                  <p className="ff_acmin fw-normal text16_xs lh_24 color_blue opacity_70 max_w_518 mb-0">
+                    Our dry ice method lifts dirt effortlessly without the need
+                    for harsh chemicals or abrasive techniques.
+                  </p>
                 </div>
-                <div>
-                    <p className='ff_acmin fw-bold text16_xs lh_24 color_blue text-center mt_lg_80 mb-md-4 mb-sm-3 mb-1'>Like What You See? Get an Instant Quote</p>
-                    <div className='mx-auto text-center max_w_172 mb-3'>
-                        <button className='ff_acmin fw-bold text16_xs lh_19 color_white btn_property btn_hover transition text-center'>Get Instant Quote</button>
-                    </div>
-
+              </div>
+              <div className="d-flex align-items-start gap-3 mb-md-3 mb-2">
+                <img src={tiksvg} alt="tiksvg" />
+                <div className="blank">
+                  <p className="ff_acmin fw-bold text16_xs lh_24 color_blue mb-md-1 mb-0">
+                    Precision Cleaning
+                  </p>
+                  <p className="ff_acmin fw-normal text16_xs lh_24 color_blue opacity_70 max_w_518 mb-0">
+                    See how accurately we target the grunge zones, ensuring
+                    every inch of your car's undercarriage and engine is
+                    meticulously cleaned.
+                  </p>
                 </div>
+              </div>
+              <div className="d-flex align-items-start gap-3 mb-md-3 mb-2">
+                <img src={tiksvg} alt="tiksvg" />
+                <div className="blank">
+                  <p className="ff_acmin fw-bold text16_xs lh_24 color_blue mb-md-1 mb-0">
+                    Safe for AlI Vehicles
+                  </p>
+                  <p className="ff_acmin fw-normal text16_xs lh_24 color_blue opacity_70 max_w_518 mb-0">
+                    Perfect for all car types, from family hatchbacks to classic
+                    cars and high-performance vehicles.
+                  </p>
+                </div>
+              </div>
+              <div className="d-flex align-items-start gap-3 mb-md-3 mb-0 pb-lg-0 pb-sm-4 pb-3">
+                <img src={tiksvg} alt="tiksvg" />
+                <div className="blank">
+                  <p className="ff_acmin fw-bold text16_xs lh_24 color_blue mb-md-1 mb-0">
+                    Eco-Friendly Approach
+                  </p>
+                  <p className="ff_acmin fw-normal text16_xs lh_24 color_blue opacity_70 max_w_518 mb-0">
+                    Notice the absence of water or chemical runoff, showcasing
+                    our commitment to an environmentally responsible cleaning
+                    process.
+                  </p>
+                </div>
+              </div>
             </div>
+            <div
+              data-aos="fade-left"
+              className="col-lg-6 d-flex justify-content-center"
+            >
+              <a href="https://youtu.be/EM_FKck0DTo?feature=shared" target='_blank'>
+                <div className="position-relative">
+                  <img
+                    className="max_w_558 max_w_sm_400 max_w_280 w-100 h-100 object-fit-contain"
+                    src={futureCleaning}
+                    alt="futureCleaning"/>
+                  <div className="position-absolute pos_playbtn2">
+                    <img
+                      className="max_w_md_60 max_w_25 cursor_pointer"
+                      src={playBtn}
+                      alt="playBtn"
+                    />
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+          <div>
+            <p className="ff_acmin fw-bold text16_xs lh_24 color_blue text-center mt_lg_80 mb-md-4 mb-sm-3 mb-1">
+              Like What You See? Get an Instant Quote
+            </p>
+            <div className="mx-auto text-center max_w_172 mb-3">
+              <button className="ff_acmin fw-bold text16_xs lh_19 color_white btn_property btn_hover transition text-center">
+                Get Instant Quote
+              </button>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    );
 
 }
 
